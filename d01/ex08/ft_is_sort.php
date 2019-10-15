9395ff312f8d816c7798be1dd19c337ac66b4c1c
Valentin LECART <valecart@student.42.fr>
2019-10-15 16:29:06 +0200

    ft_is_sort done

---
+++ d01/ex08/ft_is_sort.php
@@ -0,0 +1,14 @@
+#!/usr/bin/php
+<?PHP
+function ft_is_sort($tab) {
+    $correct = $tab;
+    sort($correct, SORT_STRING);
+    $i = 0;
+    foreach ($tab as $elem) {
+        if ($correct[$i] != $elem)
+            return FALSE;
+        $i++;
+    }
+    return TRUE;
+}
+?>
