813252ef942666a53e3d7ca49007796c96273ba2
Valentin LECART <valecart@student.42.fr>
2019-10-15 15:45:42 +0200

    split and ssap done

--- d01/ex06/ssap.php
+++ d01/ex06/ssap.php
@@ -1,5 +1,6 @@
 #!/usr/bin/php
 <?php
+
 function ft_split($str)
 {
 	$ret = explode(" ", $str);
@@ -7,5 +8,16 @@ function ft_split($str)
 	$ret = array_values($ret);
 	sort($ret, SORT_STRING);
 	return $ret;
+}
+
+foreach ($argv as $elem)
+    $clean = $clean.$elem." ";
+$clean = ft_split($clean);
+
+foreach ($clean as $elem)
+{
+    if ($i)
+        echo "$elem\n";
+    $i = 1;
 }
 ?>
