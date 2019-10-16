9fcd5c28358875133a21fe9482673f9996ea5ca2
Valentin LECART <valecart@student.42.fr>
2019-10-16 16:53:32 +0200

    fix crashes for loupe

--- d02/ex02/loupe.php
+++ d02/ex02/loupe.php
@@ -5,18 +5,20 @@ function upper_cut($tab)
 {
 	return ($tab[1].strtoupper($tab[2]).$tab[3]);
 }
-if ($argc > 1)
+
+if ($argc > 1 && file_exists($argv[1]))
 {
-	$fd = fopen($argv[1], "r");
+    if (FALSE === ($fd = fopen($argv[1], "r")))
+        return ;
 	$title = "/(<a.*title=\")(.*)(\">)/i";
 	$link = "/(<a.*>)(.*)(<\/a)/i";
 	$linkimg = "/(<a.*>)(.*)(<img)/i";
-    while (!feof($fd))
-        $line .= fgets($fd); 
+    while (FALSE !== ($line = fgets($fd)))
+        $file .= $line;
 	fclose ($fd);
-	$line = preg_replace_callback("$title", "upper_cut", $line);
-	$line = preg_replace_callback("$link", "upper_cut", $line);
-	$line = preg_replace_callback("$linkimg", "upper_cut", $line);
-	echo "$line";
+	$file = preg_replace_callback("$title", "upper_cut", $file);
+	$file = preg_replace_callback("$link", "upper_cut", $file);
+	$file = preg_replace_callback("$linkimg", "upper_cut", $file);
+	echo "$file";
 }
 ?>
