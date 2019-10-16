be73bbd822965450478b4b35becf2c48d2f9cf0e
Valentin LECART <valecart@student.42.fr>
2019-10-16 13:55:24 +0200

    add regex to test format

--- d02/ex01/one_more_time.php
+++ d02/ex01/one_more_time.php
@@ -4,6 +4,7 @@ if ($argc != 2)
     return ;
 $date = $argv[1];
 $date = explode(" ", $date);
+$tab_days = array("Lundi", "Mardi", "Mercedi", "Jeudi", "Vendredi", "Samedi", "Dimanche");
 $tab_months = array("Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre");
 $i = 1;
 foreach ($tab_months as $elem) {
@@ -11,8 +12,19 @@ foreach ($tab_months as $elem) {
         $month = $i;
     $i++;
 }
+$i = 1;
+foreach ($tab_days as $elem) {
+    if (strcasecmp($elem, $date[0]) == 0)
+        $day = $i;
+    $i++;
+}
+
 $result = $date[1]." ".$month." ".$date[3]." ".$date[4];
-if ($date2 = DateTime::createFromFormat('d m Y h:i:s', $result)){
+if (!$month || !$day || preg_match("/^([0-2][0-9]|(3)[0-1])( )(((0)[0-9])|((1)[0-2]))( )\d{4}( )([0-1][0-9]|(2)[0-3])(:)([0-5][0-9])(:)([0-5][0-9])$/", $result)."\n".$result."\n" == 0){
+    echo "Wrong Format\n";
+    return ;
+}
+if ($date2 = DateTime::createFromFormat('d m Y H:i:s', $result)){
     $date2 = $date2->format('U');
     echo "$date2\n";}
 else
