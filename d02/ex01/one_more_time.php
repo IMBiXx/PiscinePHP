bd09c0bd26494d6a59f42bd891ac661bfe1a8595
Valentin LECART <valecart@student.42.fr>
2019-10-16 12:35:45 +0200

    ex01 done + gitignore

---
+++ d02/ex01/one_more_time.php
@@ -0,0 +1,27 @@
+#!/usr/bin/php
+<?php
+if ($argc != 2)
+    return ;
+$date = $argv[1];
+$date = explode(" ", $date);
+$tab_days = array("Lundi", "Mardi", "Mercedi", "Jeudi", "Vendredi", "Samedi", "Dimanche");
+$tab_months = array("Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre");
+$i = 1;
+foreach ($tab_days as $elem) {
+    if (strcasecmp($elem, $date[0]) == 0)
+        $day = $i;
+    $i++;
+}
+$i = 1;
+foreach ($tab_months as $elem) {
+    if (strcasecmp($elem, $date[2]) == 0)
+        $month = $i;
+    $i++;
+}
+$result = $date[1]." ".$month." ".$date[3]." ".$date[4];
+if ($date2 = DateTime::createFromFormat('d m Y h:i:s', $result)){
+    $date2 = $date2->format('U');
+    echo "$date2\n";}
+else
+    echo "Wrong Format\n"
+?>
