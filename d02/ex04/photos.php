33621be2fd789e226bab4dde4571f4c27e0c48f3
Valentin LECART <valecart@student.42.fr>
2019-10-17 14:46:49 +0200

    photos done

---
+++ d02/ex04/photos.php
@@ -0,0 +1,26 @@
+#!/usr/bin/php
+<?php
+if ($argc > 1)
+    if ($content = file_get_contents($argv[1]))
+    {
+        $src = parse_url($argv[1]);
+        if (!file_exists($src['host']))
+            mkdir($src['host']);
+        $result = array();
+        preg_match_all("/(<img src=\")(.*?)(\")/", $content, $result);
+        foreach ($result[2] as $link)
+            if ($link)
+            {
+                $img = parse_url($link);
+                if (isset($img["scheme"]) && isset($img["host"]))
+                    $srcfile= $link;
+                else
+                    $srcfile = $src['scheme']."://".$src['host'].$link;
+                $path = pathinfo($link);
+                $destfile  = $src['host']."/".$path['filename'];
+                if (isset($path['extension']))
+                    $destfile = $destfile.".".$path['extension'];
+                copy($srcfile, $destfile);
+            }
+    }
+?>
