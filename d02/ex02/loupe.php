d1daabc642da1f8e1e39f6b1f6eb4c964d54e731
Valentin LECART <valecart@student.42.fr>
2019-10-17 20:16:07 +0200

    loupe fixed

--- d02/ex02/loupe.php
+++ d02/ex02/loupe.php
@@ -1,28 +1,22 @@
 #!/usr/bin/php
 <?php
-
 function upper_cut($tab)
 {
-	return ($tab[1].strtoupper($tab[2]).$tab[3]);
+   return ($tab[1].strtoupper($tab[2]).$tab[3]);
 }
-
 if ($argc > 1 && file_exists($argv[1]))
 {
-    if (FALSE === ($fd = fopen($argv[1], "r")))
-        return ;
-	$title = "/(<a.*title=\")(.*)(\">)/i";
+	$file = file_get_contents($argv[1]);
+	$title = "/(<.*title=\")(.*?)(\">)/i";
 	$link = "/(\">)(.*?)(<)/i";
-	$linkimg = "/(<a.*>)(.*)(<img)/i";
+	$spaces = "/(\">\\n)(.*)(\s.<)/i";
+	$aimg = "/(<a.*?>)(.*?)(<img)/i";
 	$span = "/(<span.*>)(.*)(<div)/i";
-	$titlespan = "/(<.*title=\")(.*)(><span)/i";
-    while (FALSE !== ($line = fgets($fd)))
-        $file .= $line;
-	fclose ($fd);
 	$file = preg_replace_callback("$title", "upper_cut", $file);
+	$file = preg_replace_callback("$aimg", "upper_cut", $file);
 	$file = preg_replace_callback("$link", "upper_cut", $file);
-	$file = preg_replace_callback("$linkimg", "upper_cut", $file);
-	$line = preg_replace_callback("$span", "upper_cut", $line);
-	$line = preg_replace_callback("$titlespan", "upper_cut", $line);
-	echo "$file";
+	$file = preg_replace_callback("$spaces", "upper_cut", $file);
+	$file = preg_replace_callback("$span", "upper_cut", $file);
+	echo $file;
 }
 ?>
