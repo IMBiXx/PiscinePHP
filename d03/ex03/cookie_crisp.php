641a0b5086f45dc330e5287623c0f547c2ecb9ae
Valentin LECART <valecart@student.42.fr>
2019-10-17 15:32:58 +0200

    cookie_crisp done

--- d03/ex03/cookie_crisp.php
+++ d03/ex03/cookie_crisp.php
@@ -1,7 +1,12 @@
 <?php
-
-foreach ($_GET as $key=>$elem)
-    $i = $key;
-
-setcookie($i, $_GET[$i],time());
+if (array_key_exists("action", $_GET)){
+    $tab = $_GET;
+    if (array_key_exists("name", $tab))
+        if ($tab['action'] == "set" && array_key_exists("value", $tab))
+            setcookie($tab['name'], $tab['value']);
+        else if ($tab['action'] == "get" && $_COOKIE[$tab['name']] != NULL)
+            echo ($_COOKIE[$tab['name']])."\n";
+        else if ($tab['action'] == "del")
+            setcookie($tab['name'], "", time() - 3600);
+}
 ?>
