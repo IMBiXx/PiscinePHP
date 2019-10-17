5551d789625b9fab74d758d5f599238dd6c42485
Valentin LECART <valecart@student.42.fr>
2019-10-17 19:48:22 +0200

    utmpx record is 628 bytes in size

--- d02/ex03/who.php
+++ d02/ex03/who.php
@@ -4,7 +4,7 @@ $utmpx = "/var/run/utmpx";
 $fd = fopen($utmpx, "r");
 $list = array();
 
-while (($bin = fread($fd, 314))){
+while (($bin = fread($fd, 628))){
     $bin = unpack("a256user/a4id/a32line/ipid/itype/I2time", $bin);
     if (strcmp($bin['type'], "7") === 0)
         array_push($list, $bin);
