8fb6bf8a1178dc55f4f07ac5a600a7b0a7e0a46b
Valentin LECART <valecart@student.42.fr>
2019-10-18 15:12:43 +0200

    create_account done

--- d04/ex01/create.php
+++ d04/ex01/create.php
@@ -1,23 +1,28 @@
 <?php
-// session_start();
-// if (isset($_POST['login']) && $_POST['login'] != NULL && isset($_POST['passwd']) && $_POST['passwd'] != NULL && isset($_POST['submit']) && $_POST['submit'] == "OK") {
-//     $_SESSION['login'] = $_POST['login'];
-//     $_SESSION['passwd'] = $_POST['passwd'];
-// }
+function validate($rep) {
+    $SUCCESS = "OK\n";
+    $ERROR = "ERROR\n";
+    if ($rep === 1)
+        echo $SUCCESS;
+    else
+        echo $ERROR;
+}
+$path = "private/";
+$file = $path."passwd";
 if (isset($_POST['login']) && $_POST['login'] != NULL && isset($_POST['passwd']) && $_POST['passwd'] != NULL && isset($_POST['submit']) && $_POST['submit'] == "OK"){
-    $path = "private/";
-    $file = "passwd";
-    if (!file_exists($path)){
+    if (!file_exists($path))
         mkdir($path);
-        file_put_contents($path.$file, "");
-    }
-    if (file_exists($path.$file)){
-        $tab = array("login" => $_POST['login'], "passwd" => hash("whirlpool", $_POST['passwd']));
-        print_r($tab);
-        file_put_contents($path.$file, serialize($tab)."\n", FILE_APPEND);
-    }
-    echo "OK\n";
+    if (!file_exists($file))
+        file_put_contents($file, null);
+    $passwd = unserialize(file_get_contents($file));
+    if ($passwd)
+        foreach ($passwd as $elem)
+            if ($elem['login'] == $_POST['login'])
+                return (validate(0));
+    $passwd[] = array("login" => $_POST['login'], "passwd" => hash('whirlpool', $_POST['passwd']));
+    file_put_contents($file, serialize($passwd));
+    return (validate(1));
 }
 else
-    echo "ERROR\n";
+    return (validate(0));
 ?>
