46c4e2146c83bdaf03cf3006cf54807f06e7ba17
Valentin LECART <valecart@student.42.fr>
2019-10-18 15:58:23 +0200

    modif account done

---
+++ d04/ex02/modif.php
@@ -0,0 +1,28 @@
+<?php
+function validate($rep) {
+    $SUCCESS = "OK\n";
+    $ERROR = "ERROR\n";
+    if ($rep === 1)
+        echo $SUCCESS;
+    else
+        echo $ERROR;
+}
+$path = "../private/";
+$file = $path."passwd";
+if (isset($_POST['login']) && $_POST['login'] != NULL && isset($_POST['oldpw']) && $_POST['oldpw'] != NULL && isset($_POST['newpw']) && $_POST['newpw'] != NULL && isset($_POST['submit']) && $_POST['submit'] == "OK"){
+    $passwd = unserialize(file_get_contents($file));
+    if ($passwd)
+        foreach ($passwd as &$elem){
+            if ($elem['login'] == $_POST['login']){
+                if ($elem['passwd'] != hash('whirlpool', $_POST['oldpw']))
+                    return (validate(0));
+                else
+                    $elem['passwd'] = hash('whirlpool', $_POST['newpw']);
+                }
+            }
+    file_put_contents($file, serialize($passwd));
+    return (validate(1));
+}
+else
+    return (validate(0));
+?>
