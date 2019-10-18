e39a5507fd375b6c7782f99688b9f4bcd6488d2f
Valentin LECART <valecart@student.42.fr>
2019-10-18 22:48:44 +0200

    ex04 done

--- d04/ex04/chat.php
+++ d04/ex04/chat.php
@@ -2,8 +2,11 @@
 
 $path = "../private/";
 $filechat = $path."chat";
+flock($filechat, LOCK_EX);
 $chat = unserialize(file_get_contents($filechat));
+flock($filechat, LOCK_UN);
+date_default_timezone_set("Europe/Paris");
 foreach ($chat as $key => $elem){
-    echo '<div><p>'.$elem["login"].' dit : '.$elem["chat"].'</p></div>';
+    echo '<div><p>['.date("H:i",$elem["time"]).'] '.$elem["login"].' dit : '.$elem["chat"].'</p></div>';
     }
 ?>
--- d04/ex04/speak.php
+++ d04/ex04/speak.php
@@ -1,18 +1,22 @@
 <?php
 session_start();
+echo"<head><script langage='javascript'>top.frames['chat'].location = 'chat.php';</script></head>";
 $path = "../private/";
 $filechat = $path."chat";
 if ($_SESSION['loggued_on_user']) {
-    echo '<form method="post" action="speak.php" style="margin-block-end: 0"><input style="width:100%; height:100%;" type="text" name="msg"></form>';
+    echo '<form method="post" action="speak.php" style="margin-block-end: 0"><input style="width:100%; height:100%;" type="text" name="msg" autofocus></form>';
     if (!file_exists($path))
         mkdir($path);
     if (!file_exists($filechat))
         file_put_contents($filechat, null);
-    else
+    else{
+        flock($filechat, LOCK_EX);
         $chat = unserialize(file_get_contents($filechat));
+    }
     if ($_POST['msg']){
     $chat[] = array("login" => $_SESSION['loggued_on_user'], "time" => time(), "chat" => $_POST['msg']);
     file_put_contents($filechat, serialize($chat));
+    flock($filechat, LOCK_UN);
 }
 }
 ?>
