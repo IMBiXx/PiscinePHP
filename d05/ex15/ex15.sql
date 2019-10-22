ae0afa249feecfdf0802a3aad592c411dc0f58f9
Valentin LECART <valecart@student.42.fr>
2019-10-22 13:02:04 +0200

    fix

--- d05/ex15/ex15.sql
+++ d05/ex15/ex15.sql
@@ -1,1 +1,1 @@
-SELECT REVERSE(SUBSTRING(`phone_number` FROM 2)) AS `enohpelet` FROM `distrib` WHERE `phone_number` LIKE '05%';
+SELECT REVERSE(SUBSTRING(`phone_number` FROM 2)) AS `rebmunenohp` FROM `distrib` WHERE `phone_number` LIKE '05%';
