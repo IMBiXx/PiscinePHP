0fef06b2af871179047d63a46a16d67c8ce58993
Valentin LECART <valecart@student.42.fr>
2019-10-23 20:27:43 +0200

    ex04

---
+++ d07/ex04/Tyrion.class.php
@@ -0,0 +1,10 @@
+<?php
+class Tyrion extends Lannister {
+    public function with($p) {
+        if (get_parent_class($p) !== "Lannister")
+            return ("Let's do this.");
+        else
+            return ("Not even if I'm drunk !");
+    }
+}
+?>
